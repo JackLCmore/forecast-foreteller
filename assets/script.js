2f5f9660e914f305fc51d914501cff88b328ff9d
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-08 14:59:46 -0700

    added each day functionality

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 $(".btn").on("click", function(event){
 event.preventDefault();
 var location = $(this).siblings(".form-control").val();
-var openWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ location + "&cnt=5&appid=24a5a13015fbcc24cbb0b4bb4df39aef"
+var openWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ location + "&cnt=37&appid=24a5a13015fbcc24cbb0b4bb4df39aef"
 fetch(openWeatherURL)
 .then(function(response){
     return response.json();
@@ -17,7 +17,7 @@ fetch(openWeatherURL)
 
     $(".history").append("<li class='list-group-item active city-bar'>"+ locationName +"</li>")
     
-    $(data.list).each(function(){
+    $(data.list[4]).each(function(){
      
     var weatherDescrip = this.weather[0].description;
     var locationDate = this.dt_txt;
@@ -25,31 +25,122 @@ fetch(openWeatherURL)
     // temp came up as kelvin, subtracting 273.15 gives back celsius
     var locationHumidity = this.main.humidity;
     var locationWindSpd = this.wind.speed;
-    
-    $(".table").find(".day").each(function(){
+
+    $(".table").find(".day1").each(function(){
         $(this).text(locationDate);
     })
-    $(".table").find(".day").siblings(".condition").each(function(){
+    $(".table").find(".condition1").each(function(){
         $(this).text(weatherDescrip);
     })
-    $(".table").find(".day").siblings(".temperature").each(function(){
+    $(".table").find(".temperature1").each(function(){
         $(this).text(locationTemp);
     })
-    $(".table").find(".day").siblings(".humidity").each(function(){
+    $(".table").find(".humidity1").each(function(){
         $(this).text(locationHumidity);
     })
-    $(".table").find(".day").siblings(".wind-speed").each(function(){
+    $(".table").find(".wind-speed1").each(function(){
         $(this).text(locationWindSpd);
     })
-    })
-    console.log(data);
-    console.log(data.list);
-    $(data.list).each(function(){
-        console.log(this.weather[0].description);
-        console.log(this.dt_txt);
-        console.log(this.main.temp);
-        console.log(this.main.humidity);
-        console.log(this.wind.speed);
-    });
+        $(data.list[12]).each(function(){
+     
+        var weatherDescrip = this.weather[0].description;
+        var locationDate = this.dt_txt;
+        var locationTemp = parseFloat(this.main.temp - 273.15).toFixed(2);
+        // temp came up as kelvin, subtracting 273.15 gives back celsius
+        var locationHumidity = this.main.humidity;
+        var locationWindSpd = this.wind.speed;
+    
+        $(".table").find(".day2").each(function(){
+            $(this).text(locationDate);
+        })
+        $(".table").find(".condition2").each(function(){
+            $(this).text(weatherDescrip);
+        })
+        $(".table").find(".temperature2").each(function(){
+            $(this).text(locationTemp);
+        })
+        $(".table").find(".humidity2").each(function(){
+            $(this).text(locationHumidity);
+        })
+        $(".table").find(".wind-speed2").each(function(){
+            $(this).text(locationWindSpd);
+        })
+        })
+            $(data.list[20]).each(function(){
+     
+            var weatherDescrip = this.weather[0].description;
+            var locationDate = this.dt_txt;
+            var locationTemp = parseFloat(this.main.temp - 273.15).toFixed(2);
+            // temp came up as kelvin, subtracting 273.15 gives back celsius
+            var locationHumidity = this.main.humidity;
+            var locationWindSpd = this.wind.speed;
+    
+            $(".table").find(".day3").each(function(){
+            $(this).text(locationDate);
+            })
+            $(".table").find(".condition3").each(function(){
+            $(this).text(weatherDescrip);
+            })
+            $(".table").find(".temperature3").each(function(){
+            $(this).text(locationTemp);
+            })
+            $(".table").find(".humidity3").each(function(){
+            $(this).text(locationHumidity);
+            })
+            $(".table").find(".wind-speed3").each(function(){
+            $(this).text(locationWindSpd);
+            })
+                $(data.list[28]).each(function(){
+        
+                var weatherDescrip = this.weather[0].description;
+                var locationDate = this.dt_txt;
+                var locationTemp = parseFloat(this.main.temp - 273.15).toFixed(2);
+                // temp came up as kelvin, subtracting 273.15 gives back celsius
+                var locationHumidity = this.main.humidity;
+                var locationWindSpd = this.wind.speed;
+            
+                $(".table").find(".day4").each(function(){
+                    $(this).text(locationDate);
+                })
+                $(".table").find(".condition4").each(function(){
+                    $(this).text(weatherDescrip);
+                })
+                $(".table").find(".temperature4").each(function(){
+                    $(this).text(locationTemp);
+                })
+                $(".table").find(".humidity4").each(function(){
+                    $(this).text(locationHumidity);
+                })
+                $(".table").find(".wind-speed4").each(function(){
+                    $(this).text(locationWindSpd);
+                })
+                    $(data.list[36]).each(function(){
+            
+                        var weatherDescrip = this.weather[0].description;
+                        var locationDate = this.dt_txt;
+                        var locationTemp = parseFloat(this.main.temp - 273.15).toFixed(2);
+                        // temp came up as kelvin, subtracting 273.15 gives back celsius
+                        var locationHumidity = this.main.humidity;
+                        var locationWindSpd = this.wind.speed;
+                    
+                        $(".table").find(".day5").each(function(){
+                            $(this).text(locationDate);
+                        })
+                        $(".table").find(".condition5").each(function(){
+                            $(this).text(weatherDescrip);
+                        })
+                        $(".table").find(".temperature5").each(function(){
+                            $(this).text(locationTemp);
+                        })
+                        $(".table").find(".humidity5").each(function(){
+                            $(this).text(locationHumidity);
+                        })
+                        $(".table").find(".wind-speed5").each(function(){
+                            $(this).text(locationWindSpd);
+                        })
+});
+});
+});
+});
 });
 });
